ceb76267aba625835c361eeb77091bb74feeffa0
s.m.zamani313@gmail.com <sobhan_117>
2023-09-14 18:58:00 +0430

    first commit

---
+++ lib/apisData/firstNewsList.dart
@@ -0,0 +1,1 @@
+String everyNews = "GET https://newsapi.org/v2/everything?q=Apple&from=2023-09-14&sortBy=popularity&apiKey=API_KEY";
--- lib/news_list_screen/newsListCubit.dart
+++ lib/news_list_screen/newsListCubit.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart';
 import 'package:programing/utils/baseCubitType.dart';
 
 enum newsEventName {
@@ -7,12 +11,35 @@ enum newsEventName {
   error,
 }
 
+class HttpService {
+  final String postsURL = "https://jsonplaceholder.typicode.com/posts";
+  String everyNews = "https://newsapi.org/v2/everything?q=Apple&from=2023-09-14&sortBy=popularity&apiKey=API_KEY";
+
+  // Future<List<User>> getPosts() async {
+  //   dynamic res = await get(Uri.parse(everyNews));
+  //
+  //   if (res.statusCode == 200) {
+  //     List<dynamic> body = jsonDecode(res.body);
+  //
+  //     List<User> posts = body
+  //         .map(
+  //           (dynamic item) => User.fromJson(item),
+  //         )
+  //         .toList();
+  //
+  //     return posts;
+  //   } else {
+  //     throw "Unable to retrieve posts.";
+  //   }
+  // }
+}
+
 class NewsListCubit extends Cubit<BaseCubitType<newsEventName>> {
   NewsListCubit(initialState) : super(initialState);
 
-  void validateNews() {
-    emit(
-      BaseCubitType(eventName: newsEventName.loaded),
-    );
-  }
+  // void getNews() {
+  //   dynamic news = Dio.emit(
+  //     BaseCubitType(eventName: newsEventName.loaded),
+  //   );
+  // }
 }
